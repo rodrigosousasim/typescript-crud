cc305e583aca3c5afca2185a94e848dd3a1f9051
Rodrigo Sousa <rodrigosousasim@gmail.com>
2026-01-14 16:14:35 -0300

    feat: api deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -30,6 +30,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
