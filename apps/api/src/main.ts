dbd6e20d6e83102d26ce71015c164f3af78beb59
Rodrigo Sousa <rodrigosousasim@gmail.com>
2026-01-14 15:08:01 -0300

    feat: api deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,35 +1,50 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common");
+const path = require("path");
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        transform: true,
+    }));
 
-  const config = new DocumentBuilder()
-    .setTitle('Typescript CRUD API')
-    .setDescription('API do projeto Typescript CRUD')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+    // SÓ CARREGA O SWAGGER SE NÃO FOR PRODUÇÃO/STAGE
+    if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'stage') {
+        const swagger_1 = require("@nestjs/swagger");
+        const fs = require("fs");
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+        const config = new swagger_1.DocumentBuilder()
+            .setTitle('Typescript CRUD API - LOCAL')
+            .setDescription('Documentação visível apenas localmente')
+            .setVersion('1.0')
+            .addBearerAuth()
+            .build();
 
-  await app.listen(3001);
+        const document = swagger_1.SwaggerModule.createDocument(app, config);
+        
+        // Setup dinâmico para rodar no http://localhost:3000/docs
+        swagger_1.SwaggerModule.setup('docs', app, document);
+        
+        console.log('Ambiente Local: Swagger ativado em /docs');
+    } else {
+        console.log('Ambiente de Servidor: Swagger desativado para performance e segurança.');
+    }
+
+    await app.listen(process.env.PORT || 3001);
 }
-bootstrap();
+
+bootstrap().catch(err => {
+    console.error('Erro no Bootstrap:', err);
+});
